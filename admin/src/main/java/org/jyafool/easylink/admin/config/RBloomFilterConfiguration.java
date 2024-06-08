8cad6787e0839ebb60e27b8634e9054070c14a65
GIMME <3086660647@qq.com>
2024-06-08 12:31:21 +0800

    feat(admin): 开发数据库、布隆过滤器、mp数据填充、全局异常处理器以及工具类相关配置

---
+++ admin/src/main/java/org/jyafool/easylink/admin/config/RBloomFilterConfiguration.java
@@ -0,0 +1,36 @@
+package org.jyafool.easylink.admin.config;
+
+import org.redisson.api.RBloomFilter;
+import org.redisson.api.RedissonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 布隆过滤器配置
+ * @Author jyafool
+ * @Version 1.0
+ * @Since 2024/6/7
+ */
+@Configuration(value = "rBloomFilterConfigurationByAdmin")
+public class RBloomFilterConfiguration {
+
+    /**
+     * 防止用户注册查询数据库的布隆过滤器
+     */
+    @Bean
+    public RBloomFilter<String> userRegisterCachePenetrationBloomFilter(RedissonClient redissonClient) {
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("userRegisterCachePenetrationBloomFilter");
+        cachePenetrationBloomFilter.tryInit(100000000L, 0.001);
+        return cachePenetrationBloomFilter;
+    }
+
+    /**
+     * 防止分组标识注册查询数据库的布隆过滤器
+     */
+    @Bean
+    public RBloomFilter<String> gidRegisterCachePenetrationBloomFilter(RedissonClient redissonClient) {
+        RBloomFilter<String> cachePenetrationBloomFilter = redissonClient.getBloomFilter("gidRegisterCachePenetrationBloomFilter");
+        cachePenetrationBloomFilter.tryInit(200000000L, 0.001);
+        return cachePenetrationBloomFilter;
+    }
+}
